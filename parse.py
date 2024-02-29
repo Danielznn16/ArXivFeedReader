f2e7c0ad414f35d0f0319fa11c77048d2313af1b
Daniel Zhang-Li <zlnn17@tsinghua.org.cn>
2024-02-29 11:48:23 +0800

    push

--- parse.py
+++ parse.py
@@ -10,41 +10,42 @@ def clean_html(html_content):
 	soup = BeautifulSoup(html_content, "html.parser")
 	return html.unescape(soup.get_text())
 
-def convert_title_string(title_string):
-	# Extracting the title content up to the arXiv identifier
-	match = re.match(r"(.+?)\s\((arXiv:.+?)\s\[(.+?)\].*?\)", title_string)
-	if match:
-		title, _, tag = match.groups()
-	return (title, tag)
-
 
 def clean_entry(feed_entry):
-	feed_entry['author'] = clean_html(feed_entry['author'])
-	feed_entry['summary'] = clean_html(feed_entry['summary']).replace("\n- ","").replace("\n"," ").replace("  ", " ")
-	feed_entry["title"], feed_entry["tag"] = convert_title_string(feed_entry.title)
-	del feed_entry["summary_detail"], feed_entry["links"], feed_entry["title_detail"], feed_entry["author_detail"], feed_entry["authors"]
-	return feed_entry
+	cleaned_entry = dict()
+	cleaned_entry["title"] = clean_html(feed_entry.title)
+	cleaned_entry['authors'] = clean_html(feed_entry.author_detail["name"])
+	summary_header, abstract = feed_entry.summary.split("\n")[0], "\n".join(feed_entry.summary.split("\n")[1:])
+	cleaned_entry['abstract'] = abstract
+	cleaned_entry["link"] = feed_entry.link
+	cleaned_entry["tag"] = [tag["term"] for tag in feed_entry.tags]
+	cleaned_entry["id"] = feed_entry.id.split("arXiv.org:")[-1]
+	cleaned_entry["status"] = summary_header.split("Announce Type: ")[-1].strip()
+	# feed_entry["title"], feed_entry["tag"] = convert_title_string(feed_entry.title)
+	# del feed_entry["summary_detail"], feed_entry["links"], feed_entry["title_detail"], feed_entry["author_detail"], feed_entry["authors"]
+	return cleaned_entry
 
 def convert_data(source_data):
 	target_data = {}
 	
 	target_data['link'] = source_data['link']
-	target_data['abstract'] = source_data['summary']
-	target_data['authors'] = source_data['author']
+	target_data['abstract'] = source_data['abstract']
+	target_data['authors'] = source_data['authors']
 	
 	# Convert the tags into the desired format
-	target_data[source_data["tag"]] = True
+	for tag in source_data['tag']:
+		target_data[tag] = True
 	# target_data['cs'] = {source_data['tag']: True}
 	
 	# Convert the time.struct_time into the desired string format
-	updated_date = time.strftime('%a, %d %b %Y %H:%M:%S GMT', source_data['updated'])
+	updated_date = time.strftime('%a, %d %b %Y %H:%M:%S GMT', source_data['date'])
 	target_data['date'] = updated_date
 	
 	email_date = time.strftime('%Y-%m-%d', source_data['email_date'])
 	target_data['email_date'] = email_date
 	
 	# Transform the id into the desired format
-	arxiv_id = source_data['id'].split('/')[-1]
+	arxiv_id = source_data['id']
 	target_data['id'] = f"arXiv:{arxiv_id}"
 	
 	target_data['title'] = source_data['title']
@@ -57,27 +58,25 @@ collection = client.mail.arxiv
 
 from pprint import pprint
 # Parse the RSS feed
-rss_feed_urls = ['https://export.arxiv.org/rss/cs.CL']
+rss_feed_urls = ['http://rss.arxiv.org/rss/cs.CL']
 def parse():
 	for rss_feed_url in rss_feed_urls:
 		feed = feedparser.parse(rss_feed_url)
 		time = feed.feed.updated_parsed
 		for entry in feed.entries:
-			try:
-				raw_title = entry.title
+			if True:
 				entry = clean_entry(entry)
 				entry["date"] = time
 				entry["email_date"] = time
+				status = entry["status"]
 				entry = convert_data(entry)
-				entry["ParserVer"] = "2.0"
-				if entry["id"]+"v1 " not in raw_title:
-					del entry["email_date"]
-					# print(raw_title)
-				else:
-					print(entry["id"])
-				collection.update_one(dict(id=entry["id"]),{"$set":entry},upsert=True)
-			except:
-				pass
+				entry["ParserVer"] = "2.1"
+				if status!="new":
+					del entry["email_date"], entry["date"]
+				pprint(entry)
+				# collection.update_one(dict(id=entry["id"]),{"$set":entry},upsert=True)
+			# except:
+			# 	pass
 
 parse()
 
@@ -86,4 +85,4 @@ while True:
 		parse()
 	except:
 		pass
-	time.sleep(4 * 60 * 60)  # Sleeps for 4 hours
+	time.sleep(23 * 60 * 60)  # Sleeps for 4 hours
